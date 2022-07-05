d6e2b75e97dc6db44e60140ec02533a7382876f0
Joao Aroeira <j.aroeira@gmail.com>
2022-07-05 17:42:57 +0000

    add api tvshows endpoints

--- API/Controllers/TVShowsController.cs
+++ API/Controllers/TVShowsController.cs
@@ -1,7 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
 {
     public class TVShowsController : BaseApiController
     {
+        public TVShowsController()
+        {
+        }
 
+        [HttpGet]
+        public ActionResult GetTVShows()
+        {
+            return Ok("GetTVShows!!!!");
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetTVShowById(int id)
+        {
+            return Ok($"GetTVShow with id: {id}");
+        }
+
+        [HttpPost]
+        public ActionResult CreateTVShow()
+        {
+            return Ok("Create TVShows!!!!");
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateTVShow(int id)
+        {
+            return Ok($"Update TVShows!!!! with id: {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteTVShow(int id)
+        {
+            return Ok($"Delete TVShows!!!! with id: {id}");
+        }
     }
 }
